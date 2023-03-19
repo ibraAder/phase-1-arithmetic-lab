24fe3643ee71cb589c7f019448cfb5f4137da735
ibraAder <127183391+ibraAder@users.noreply.github.com>
2023-03-19 11:26:37 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+let number="multiply";
+num1=31;
+num2=2;
+console.log(num1 * num2);
+
+const digit="random";
+Math.floor(Math.random() > 0);
+
+let num="mod";
+num3=12;
+num4=8;
+console.log(num3%num4);
+
+let find="max";
+Math.max(2,4,6,8,10,12,14,14,16,18,20);
+
